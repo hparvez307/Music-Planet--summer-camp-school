2d0afda512f12314771c3344c4dbc5b3f7988cd1
hparvez307 <hparvez307@gmail.com>
2023-06-07 00:59:43 +0600

    footer done

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -3,27 +3,18 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <div>
-            <div className='hero  bg-[url("https://i.ibb.co/QkpgvVb/image-2.png")]     h-[90vh]    '>
+        <div className='hero  bg-[url("https://i.ibb.co/QkpgvVb/image-2.png")] h-[90vh] max-[600px]:h-[120vh]'>
 
-                <div className='w-full bg-gradient-to-b h-full from-[#f1f1f1]  space-y-5  text-center '>
-                    <h1 className='text-7xl md:pt-28  pt-8 text-bold tracking-wider   '>Music Planet</h1>
+            <div className='w-full bg-gradient-to-b h-full from-[#f1f1f1]  space-y-5  text-center '>
+                <h1 className='text-7xl md:pt-28  pt-8 text-bold tracking-wider   '>Music Planet</h1>
 
+                <h1 className='text-4xl  text-red-600 pt-8 text-bold tracking-wider  '>Learn Music with Professionals</h1>
+                <p className='md:w-8/12 max-[600px]:px-4  font-bold mx-auto'>
+                    Music Planet is a prestigious music school that has been nurturing talent and inspiring musicians since its inception. Situated in the heart of a bustling city, Music Planet is a haven for aspiring musicians of all ages and skill levels. With a mission to ignite the passion for music in every individual, this renowned institution has become synonymous with excellence in musical education. At Music Planet, the atmosphere is vibrant and pulsating with creativity.
+                    <br />
 
-
-                    <h1 className='text-4xl  text-red-600 pt-8 text-bold tracking-wider  '>Learn Music with Professionals</h1>
-                    <p className='md:w-8/12 font-bold mx-auto'>
-                        Music Planet is a prestigious music school that has been nurturing talent and inspiring musicians since its inception. Situated in the heart of a bustling city, Music Planet is a haven for aspiring musicians of all ages and skill levels. With a mission to ignite the passion for music in every individual, this renowned institution has become synonymous with excellence in musical education. At Music Planet, the atmosphere is vibrant and pulsating with creativity.
-                        <br />
-
-                         The dedicated faculty consists of highly skilled instructors who are not only accomplished musicians themselves but also passionate about teaching. They offer a wide range of music programs, including instrumental and vocal training, music theory, composition, and ensemble classes. Whether one desires to master the piano, guitar, violin, drums, or explore their vocal abilities, Music Planet provides a comprehensive curriculum tailored to individual aspirations.
-                    </p>
-                </div>
-
-                {/* <div className='w-full max-[571px]:pt-8'>
-                    <Lottie animationData={car} loop={true} />
-                </div> */}
-
+                    The dedicated faculty consists of highly skilled instructors who are not only accomplished musicians themselves but also passionate about teaching. They offer a wide range of music programs, including instrumental and vocal training, music theory, composition, and ensemble classes. Whether one desires to master the piano, guitar, violin, drums, or explore their vocal abilities, Music Planet provides a comprehensive curriculum tailored to individual aspirations.
+                </p>
             </div>
         </div>
     );
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,85 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaMapMarkerAlt, FaCarSide, FaRegEnvelope, FaPhoneAlt } from 'react-icons/fa';
+
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className=' footer-container  bg  mt-20 '>
+
+            <div className='flex pb-5 pt-16 px-8 flex-col lg:flex-row max-[600px]:text-center justify-between '>
+
+                <div>
+
+                    <div className='flex items-center'>
+                        <Link><img className='h-14 max-[571px]:pl-20' src="https://i.ibb.co/CnHwkS5/logo-2.png" alt="" /></Link>
+
+                        <Link to="/" ><h1 className='text-4xl  font-extrabold  tracking-wider'>TOY<span className='' >LAND</span></h1></Link>
+                    </div>
+
+                    <p className='my-4'>Find The best toy cars with reasonable  <br />price for kids from our website.</p>
+
+                    <p>Find us on social media:</p>
+                    <img className='mt-1 max-[571px]:pl-32' src="https://i.ibb.co/BG7QcdW/Group-9969.png" alt="" />
+
+                </div>
+
+                {/* policies */}
+                <div className='max-[571px]:mb-10 max-[571px]:mt-10'>
+                    <h1 className='text-2xl my-3  font-bold '>Policies</h1>
+                    <div className='space-y-1'>
+                        <p>ToyLand Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Refund policy</p>
+                        <p>Payment policy</p>
+
+                    </div>
+                </div>
+                {/* important links */}
+                <div className='max-[571px]:mb-10'>
+                    <h1 className='text-2xl my-3  font-bold '>Important Links</h1>
+                    <div className='space-y-1'>
+                        <p>About Us</p>
+                        <p>Blog</p>
+                        <p>Contact Us</p>
+                        <p>Shipping Method</p>
+                        <p>Order Tracking</p>
+
+                    </div>
+                </div>
+
+                {/* contact info */}
+                <div >
+                    <h1 className='text-2xl my-3 font-bold '>Contact Us</h1>
+                    <div className='flex max-[571px]:pl-14  gap-2'>
+                        <div className='text-2xl mt-2'>
+                            < FaMapMarkerAlt />
+                        </div>
+                        <p>H#05, RD#05, kaderabad housing, <br /> Mohammadpur, Dhaka-1207, Bangladesh </p>
+                    </div>
+
+                    <div className='flex max-[571px]:pl-14 my-4 gap-2'>
+                        <div className='text-2xl '>
+                            < FaRegEnvelope />
+                        </div>
+                        <p>hparvez307@gmail.com</p>
+                    </div>
+
+                    <div className='flex max-[571px]:pl-14 gap-2'>
+                        <div className='text-2xl mt-1'>
+                            < FaPhoneAlt />
+                        </div>
+                        <p>+8801838634389</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* divider */}
+            <div className="divider text-7xl text-gray-500"><FaCarSide /> </div>
+
+            <div className='pb-9 text-center pt-4'>
+                <p> <span className='text-bold'>&copy;</span> 2023 ToyLand. All Rights Reserved</p>
+            </div>
         </div>
     );
 };
