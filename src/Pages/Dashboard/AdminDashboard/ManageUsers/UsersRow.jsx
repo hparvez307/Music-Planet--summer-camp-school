c73bb65c62b3e4d8dad20f0f681734a878f34d0c
hparvez307 <hparvez307@gmail.com>
2023-06-08 15:32:30 +0600

    make instructor

--- src/Pages/Dashboard/AdminDashboard/ManageUsers/UsersRow.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageUsers/UsersRow.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-const UsersRow = ({sl, user, refetch}) => {
+const UsersRow = ({ sl, user, refetch }) => {
 
-    const handleMakeAdmin = () => {
-        
+
+    // handle make admin
+    const handleMakeAdmin = (id) => {
+
+        fetch(`https://music-planet-server.vercel.app/makeAdmin/${id}`, {
+            method: 'PATCH',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('access-token')}`
+
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refetch();
+            })
+
     }
-    const handleMakeInstructor = () => {
+
+
+    // handle make instructor
+    const handleMakeInstructor = (id) => {
+
+
+        fetch(`https://music-planet-server.vercel.app/makeInstructor/${id}`, {
+            method: 'PATCH',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('access-token')}`
+
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refetch();
+            })
 
     }
 
@@ -13,18 +45,18 @@ const UsersRow = ({sl, user, refetch}) => {
 
     return (
         <tr >
-        <th>{sl}</th>
-        <td>{user?.name}</td>
-        <td>{user?.email}</td>
-        <td>{user?.role}</td>
-        <td>{
-            
-                <button disabled={user.role === 'admin'} onClick={() => handleMakeAdmin()} className="btn bg-green-600 text-white btn-ghost btn-md ">Make Admin</button>
-        }</td>
-        <td>
-            <button disabled={user.role === 'instructor'} onClick={() => handleMakeInstructor()} className="btn bg-orange-700 text-white btn-ghost btn-md ">Make Instructor</button>
-        </td>
-    </tr>
+            <th>{sl}</th>
+            <td>{user?.name}</td>
+            <td>{user?.email}</td>
+            <td>{user?.role}</td>
+            <td>{
+
+                <button disabled={user.role === 'admin'} onClick={() => handleMakeAdmin(user?._id)} className="btn bg-green-600 text-white btn-ghost btn-md ">Make Admin</button>
+            }</td>
+            <td>
+                <button disabled={user.role === 'instructor'} onClick={() => handleMakeInstructor(user?._id)} className="btn bg-orange-700 text-white btn-ghost btn-md ">Make Instructor</button>
+            </td>
+        </tr>
     );
 };
 
