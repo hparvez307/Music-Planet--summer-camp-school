90d14e20c0da1074964025fc54623452cce8a92d
hparvez307 <hparvez307@gmail.com>
2023-06-09 11:29:51 +0600

    add class

--- src/Pages/Dashboard/InstructorDashboard/AddAClass/AddAClass.jsx
+++ src/Pages/Dashboard/InstructorDashboard/AddAClass/AddAClass.jsx
@@ -9,14 +9,14 @@ const AddAClass = () => {
         register,
         formState: { errors },
         handleSubmit,
-        
+
     } = useForm();
 
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
 
-    const handleAddToy = (data) => {
+    const handleAddClass = (data) => {
         // fetch('https://toy-land-server.vercel.app/addAToy', {
         //     method: 'POST',
         //     headers: {
@@ -49,125 +49,96 @@ const AddAClass = () => {
 
 
 
-            <form className='container w-10/12 mx-auto p-10' onSubmit={handleSubmit(handleAddToy)}>
+            <form className='container w-10/12 mx-auto p-10' onSubmit={handleSubmit(handleAddClass)}>
 
 
-<div className='md:flex justify-between gap-10'>
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Toy Photo Url</span>
-            <input className="input px-3 input-bordered"
-                {...register('photo', { required: true })}
-                type='url'
-                placeholder='Photo Url'
-            /> </label>
-    </div>
+                <div className='md:flex justify-between gap-14'>
+                    <div className="my-4 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Class Name</span>
+                            <input className="input px-4 input-bordered"
+                                {...register('className', { required: true })}
+                                type='text'
+                                placeholder='Class Name'
+                            /> </label>
+                    </div>
 
 
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Toy Name</span>
-            <input className="input px-3 input-bordered"
-                {...register('name', { required: true })}
-                type='text'
-                placeholder='Toy Name'
-            /> </label>
-    </div>
-</div>
+                    <div className="my-4 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Class Image</span>
+                            <input className="input px-4 input-bordered"
+                                {...register('image', { required: true })}
+                                type='file'
+                                placeholder='image'
+                            /> </label>
+                    </div>
+                </div>
 
 
-<div className='md:flex justify-between gap-10'>
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Seller Name</span>
-            <input className="input px-3 input-bordered"
-                {...register('sellerName')}
-                type='text'
-                defaultValue={user?.displayName}
-            /> </label>
-    </div>
+                <div className='md:flex justify-between gap-14'>
+                    <div className="my-5 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Instructor Name</span>
+                            <input className="input px-3 input-bordered"
+                                {...register('instructorName')}
+                                type='text'
+                                readOnly
+                                defaultValue={user?.displayName}
+                            /> </label>
+                    </div>
 
 
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Seller Email</span>
-            <input className="input px-3 input-bordered"
-                {...register('sellerEmail')}
-                type='text'
-                defaultValue={user?.email}
-            /> </label>
-    </div>
-</div>
-
-
-
-
-
-<div className='md:flex justify-between gap-10'>
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Sub Category</span>
-            <input className="input px-3 input-bordered"
-                {...register('subCategory', { required: true })}
-                type='text'
-                placeholder='Sub Category'
-            /> </label>
-    </div>
-
-
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Price</span>
-            <input className="input px-3 input-bordered"
-                {...register('price', { required: true })}
-                type='number'
-                max={1000}
-                placeholder='Price'
-            /> </label>
-    </div>
-</div>
-
-
-<div className='md:flex justify-between gap-10'>
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Rating</span>
-            <input className="input px-3 input-bordered"
-                {...register('rating', { required: true })}
-                type='text'
-                placeholder='Rating'
-            /> </label>
-    </div>
-
-
-    <div className="my-6 w-full">
-        <label className="input-group input-group-vertical">
-            <span>Available Quantity</span>
-            <input className="input px-3 input-bordered"
-                {...register('quantity', { required: true })}
-                type='number'
-                placeholder='Available Quantity'
-            /> </label>
-    </div>
-</div>
+                    <div className="my-5 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Instructor Email</span>
+                            <input className="input px-4 input-bordered"
+                                {...register('instructorEmail')}
+                                type='text'
+                                readOnly
+                                defaultValue={user?.email}
+                            /> </label>
+                    </div>
+                </div>
 
 
 
-<div className="my-6 w-full">
-    <label className="input-group input-group-vertical">
-        <span>Detail Description</span>
-        <input className="input px-3 input-bordered"
-            {...register('description', { required: true })}
-            type='text'
-            placeholder='Detail Description'
-        /> </label>
-</div>
+
+
+                <div className='md:flex justify-between gap-14'>
+                    <div className="my-6 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Available Seats</span>
+                            <input className="input px-3 input-bordered"
+                                {...register('availableSeats', { required: true })}
+                                type='text'
+                                placeholder='Available Seats'
+                            /> </label>
+                    </div>
+
+
+                    <div className="my-6 w-full">
+                        <label className="input-group input-group-vertical">
+                            <span className='text-red-600 bg-black  text-bold'>Price</span>
+                            <input className="input px-3 input-bordered"
+                                {...register('price', { required: true })}
+                                type='number'
+                                placeholder='Price'
+                            /> </label>
+                    </div>
+                </div>
 
 
 
-<button className='btn btn-block'><input type="submit" value='Add Toy' className='text-3xl' /></button>
 
-</form>
+
+
+
+
+
+                <button className='btn btn-block text-red-600 bg-black'><input type="submit" value='Add Class' className='text-3xl' /></button>
+
+            </form>
 
 
         </>
