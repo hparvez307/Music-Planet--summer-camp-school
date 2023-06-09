d0cdc48fa3f22455907ab515852ea273b626e350
hparvez307 <hparvez307@gmail.com>
2023-06-09 14:23:21 +0600

    add classes

--- src/Pages/Dashboard/InstructorDashboard/AddAClass/AddAClass.jsx
+++ src/Pages/Dashboard/InstructorDashboard/AddAClass/AddAClass.jsx
@@ -1,43 +1,66 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../../../Providers/AuthProviders';
+import Swal from 'sweetalert2';
 
 const AddAClass = () => {
 
 
     const {
         register,
-        formState: { errors },
-        handleSubmit,
-
+        handleSubmit
     } = useForm();
 
 
     const { user } = useContext(AuthContext);
 
 
-    const handleAddClass = (data) => {
-        // fetch('https://toy-land-server.vercel.app/addAToy', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(data)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data);
-        //         if (data.insertedId) {
-        //             Swal.fire({
-        //                 title: 'Success!',
-        //                 text: 'Successfully Added Toy Car',
-        //                 icon: 'success',
-        //                 confirmButtonText: 'Ok'
-        //             })
-        //         }
-        //     })
 
-        console.log(data)
+    const handleAddClass = (data) => {
+
+
+        const img_hosting_url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_Imagebb_Upload_Token}`;
+        const formData = new FormData();
+        formData.append('image', data.image[0]);
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                const imageUrl = imgData.data.display_url;
+                
+           const classData = {
+            ...data,
+            image: imageUrl,
+            status: 'pending'
+            
+           }
+           console.log(classData)
+
+
+        fetch('https://toy-land-server.vercel.app/addAToy', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(classData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Successfully Added Class',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    })
+                }
+            })
+
+    })
     }
 
 
@@ -67,7 +90,7 @@ const AddAClass = () => {
                     <div className="my-4 w-full">
                         <label className="input-group input-group-vertical">
                             <span className='text-red-600 bg-black  text-bold'>Class Image</span>
-                            <input className="input px-4 input-bordered"
+                            <input className="input px-4 pt-2 input-bordered"
                                 {...register('image', { required: true })}
                                 type='file'
                                 placeholder='image'
