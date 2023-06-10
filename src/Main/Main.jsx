f1979d92816a8281a0cc9b8b243667c8f1101016
hparvez307 <hparvez307@gmail.com>
2023-06-11 03:48:11 +0600

    title

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nabvar from '../Pages/Shared/Nabvar/Nabvar';
 import Footer from '../Pages/Shared/Footer/Footer';
@@ -7,21 +7,9 @@ import { Helmet } from 'react-helmet';
 import { motion, useScroll, useSpring } from "framer-motion";
 
 const Main = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
-    useEffect(() => {
-      const body = document.body;
-      if (isDarkMode) {
-        document.body.classList.add('dark-mode');
-      } else {
-        document.body.classList.remove('dark-mode');
-      }
-    }, [isDarkMode]);
   
-    const toggleDarkMode = () => {
-      setIsDarkMode(!isDarkMode);
-    };
 
+ 
     const { scrollYProgress } = useScroll();
     const scaleX = useSpring(scrollYProgress, {
       stiffness: 100,
@@ -36,9 +24,6 @@ const Main = () => {
             </Helmet>
             <motion.div className="progress-bar" style={{ scaleX }} />
             <Nabvar></Nabvar>
-            <button onClick={toggleDarkMode}>
-          {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-        </button>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
